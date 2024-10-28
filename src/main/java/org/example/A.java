55db6a95e440f82dde89267d80b64cbe777b6d76
qzhang3321 <qzhang897-c@my.cityu.edu.hk>
2024-10-28 19:56:13 +0800

    add dict

---
+++ src/main/java/org/example/A.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class A {
+    private int a;
+    private int b;
+    public A(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+    public int getB(int b) {
+        return b;
+    }
+    public int sum() {
+        return a + b;
+    }
+}
---
+++ src/test/java/testCases/Test1.java
@@ -0,0 +1,14 @@
+package testCases;
+
+import org.example.A;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test1 {
+
+    @Test
+    void testSum(){
+        assertEquals(5, new A(2,3).sum());
+    }
+}
